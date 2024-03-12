19f68440c972284d81292a513eb39d87191bd9e8
inunice <ammanguan@up.edu.ph>
2024-03-12 22:52:05 +0800

    chore: add font 'Bagnard' to project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'bagnard': ['Bagnard', 'sans-serif'], // Add your font name here
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
