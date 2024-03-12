07def8a7304497659068eea5a8374d3d391c75c4
inunice <ammanguan@up.edu.ph>
2024-03-13 02:05:20 +0800

    feat: style header section

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,11 @@ import "@/app/globals.css";
 export default async function NavBar() {
   return (
     <div className="bg-eggshell max-w-full h-screen flex flex-col items-center justify-center">
-      <p className="text-[70px] font-bagnard text-persian-red">
+      <p className="text-[70px] font-bagnard text-persian-red mb-10">
         crafting code, weaving stories.
       </p>
-      <p className="text-[30px] font-bagnard text-reseda-green">
-        hi, i'm ayen!
+      <p className="text-[45px] font-bagnard text-eerie-black">
+        hi, i'm <span className="text-reseda-green">ayen</span>!
       </p>
       <p className="max-w-[600px] text-lg text-eerie-black text-center">
         I'm an aspiring full-stack developer with a passion for UI/UX and
