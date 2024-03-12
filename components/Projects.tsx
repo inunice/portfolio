0a5bb3cfbaedb8588a4dd5348e3e447bf9154af6
inunice <ammanguan@up.edu.ph>
2024-03-13 02:13:25 +0800

    feat: style projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,21 +2,21 @@ import "@/app/globals.css";
 
 export default async function NavBar() {
   return (
-    <div className="bg-bistre max-w-full h-screen flex flex-col items-center justify-center text-eggshell">
+    <div className="bg-bistre max-w-full h-[60vh] flex flex-col items-center justify-center text-eggshell">
       <h1>why do stories matter?</h1>
-      <p>
+      <p className="w-[65vh] text-center mb-5">
         I've always had a knack and deep appreciation for all forms of
         storytelling, of how they unconsciously touch the lives of those who
         consume it, but stories don't just live in adventure novels or musical
         notes.
       </p>
-      <p>
+      <p className="w-[65vh] text-center mb-10">
         With every click, there's a well-thought-out tale, a connection waiting
         to be made. My journey, being both a layout artist and a computer
         science student, shapes my perspective on weaving compelling narratives
         in the realm of development.
       </p>
-      <h2>Projects</h2>
+      <h2 className="">Projects</h2>
     </div>
   );
 }
