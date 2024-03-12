1f4b2379fdc7e55689ed530343220106948d505f
inunice <ammanguan@up.edu.ph>
2024-03-12 23:16:17 +0800

    feat: add header content

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,17 @@
 export default async function NavBar() {
   return (
-    <div>
-      <p className="text-2xl font-bagnard">crafting code, weaving stories.</p>
+    <div className="bg-eggshell w-screen h-screen flex flex-col items-center justify-center">
+      <p className="text-[70px] font-bagnard text-persian-red">
+        crafting code, weaving stories.
+      </p>
+      <p className="text-[30px] font-bagnard text-reseda-green">
+        hi, i'm ayen!
+      </p>
+      <p className="max-w-[600px] text-lg text-eerie-black text-center">
+        I'm an aspiring full-stack developer with a passion for UI/UX and
+        front-end magic! Underneath a project's hundreds of lines of code lies a
+        story waiting to be told — let's bring yours to life.
+      </p>
     </div>
   );
 }
