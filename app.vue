13676b8534c416f7f4ddde6599070cc5478b92fc
inunice <ammanguan@up.edu.ph>
2025-06-06 13:30:42 +0800

    chore: add analytics tracking

--- app.vue
+++ app.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
+
 <template>
+  <Analytics />
   <Header />
   <div class="flex flex-col gap-30 mb-30">
     <Projects />
